7badd37344fe68486fd22b83adee8b89b36f2516
lukaszbrzoska1 <lukaszbrzoska1@gmail.com>
2023-01-05 00:18:55 +0100

    implementing service fetching topCharts from shazamCore , creating Discover page displaing an image, title and subtitle of the song

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
-// import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import { shazamCoreApi } from './services/shazamCore';
 
-// export const store = configureStore({
-//   reducer: {
+export const store = configureStore({
+  reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
 
-//   }
-// })
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(shazamCoreApi.middleware),
+});
